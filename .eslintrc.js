c9a252385c27d99843ea89ef723914eec43f7b9f
Jun Shindo <jayes.mobile@gmail.com>
2019-03-11 00:11:30 +0900

    Prettier追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
+  extends: [
+    'plugin:vue/recommended',
     '@vue/standard',
+    '@vue/prettier',
     '@vue/typescript'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@vue/prettier': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      require('./.prettierrc')
+    ]
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
