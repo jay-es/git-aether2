abaa3dc2b6bf05ce5f6d589204c7bcbfd4a4fdf8
Jun Shindo <jayes.mobile@gmail.com>
2019-05-03 00:15:57 +0900

    ファイル変更時の処理をdebounce化

--- src/scripts/watch.ts
+++ src/scripts/watch.ts
@@ -1,9 +1,14 @@
 import { watch } from 'fs'
 
-export default (basePath: string, callback: () => void) =>
-  watch(basePath, { recursive: true }, (eventType, filename) => {
+export default (basePath: string, callback: () => void) => {
+  let timerId = 0
+
+  return watch(basePath, { recursive: true }, (eventType, filename) => {
     // Gitディレクトリは除外（startsWith だと .gitignore なども弾かれてしまうので正規表現）
     if (/^\.git(\/|\\|$)/.test(filename)) return
 
-    callback()
+    // ブランチ切替時など、大量のファイル変更があると固まるので debounce
+    clearTimeout(timerId)
+    timerId = window.setTimeout(callback, 10)
   })
+}
